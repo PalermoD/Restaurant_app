f1de5fd76347d29ac9f7f569f6deb3906b426489
PalermoD <palermo.deschamps@gmail.com>
2014-10-14 14:43:06 -0400

    Party crud routes added and working

--- Restaurant/app.rb
+++ Restaurant/app.rb
@@ -6,8 +6,10 @@ ActiveRecord::Base.establish_connection(
 	:database => "restaurant_db"
 );        
 
+ROOT_PATH = Dir.pwd
+Dir[ROOT_PATH+"/models/*.rb"].each{ |file| require file }
 
-require_relative 'models/food'
+
 
 get '/' do 
 	"welcome to the best FU#KING restarant app EVER!!!"
@@ -15,12 +17,12 @@ end
 
 get '/foods' do 
    @foods = Food.all 
-   erb :index 
+   erb :'foods/index'  
 end 
 
 
 get '/foods/new' do 
-   erb :new 
+   erb :'foods/new' 
 end 
 
 post '/foods' do 
@@ -28,17 +30,17 @@ post '/foods' do
 	new_price = params['cusine_price']
 	new_allergens = params['cusine_allergens']
 	Food.create({cusine_type: new_cusine, price: new_price, allergens: new_allergens })
-	redirect '/profiles'
+	redirect '/foods'
 end 
 
 get '/foods/:id' do 
 	@foods = Food.find(params[:id])
-	erb :show  
+	erb :'foods/show'  
 end 
 
 get '/foods/:id/edit' do 
 	@foods = Food.find(params[:id])
-	erb :edit 
+	erb :'foods/edit' 
 end 
 
 patch '/foods/:id' do 
@@ -48,8 +50,54 @@ patch '/foods/:id' do
 	redirect '/foods'
 end 
 
-delete '/profiles/:id' do 
+delete '/foods/:id' do 
 	food = Food.find(params[:id])
 	food.destroy 
 	redirect '/foods'
 end 
+
+
+#Party time 
+get '/parties' do 
+   @parties = Party.all 
+   erb :'parties/index'  
+end 
+
+
+get '/parties/new' do 
+   erb :'parties/new' 
+end 
+
+post '/parties' do 
+	new_table = params['table_number']
+	new_guest = params['guest_number']
+	paid = params['paid']
+	Party.create({table_number: new_table, guest_number: new_guest, paid: paid })
+	redirect '/parties'
+end 
+
+get '/parties/:id' do 
+	@parties = Party.find(params[:id])
+	erb :'parties/show'  
+end 
+
+get '/parties/:id/edit' do 
+	@parties = Party.find(params[:id])
+	erb :'parties/edit' 
+end 
+
+patch '/parties/:id' do 
+	party = Party.find(params[:id])
+	new_cusine = params['cusine_name']
+	profile.update({cusine_type: new_cusine})
+	redirect '/parties'
+end 
+
+delete '/parties/:id' do 
+	party = Party.find(params[:id])
+	party.destroy 
+	redirect '/parties'
+end 
+
+
+
